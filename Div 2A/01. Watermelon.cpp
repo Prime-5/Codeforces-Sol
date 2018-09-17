1eb7b1864bf78e07ccffd0a897529f730ed567c1
Abhishek Pratap Singh <35230163+Prime-5@users.noreply.github.com>
2018-09-17 09:35:06 +0530

    Create 01. Watermelon.cpp

---
+++ Div 2A/01. Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2)
+    {
+       cout<<"YES";
+       return 0;
+    }
+
+    cout<<"NO";
+    return 0;
+    
+}
