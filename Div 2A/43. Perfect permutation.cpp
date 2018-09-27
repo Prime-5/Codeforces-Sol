345c9d098681c5580d905f3dac15397c0851da4a
Abhishek Pratap Singh <35230163+Prime-5@users.noreply.github.com>
2018-09-27 07:05:07 +0530

    Add files via upload

---
+++ Div 2A/43. Perfect permutation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,i;
+	cin>>n;
+	if(n%2==1)
+	{	cout<<"-1";
+		return 0;
+	}
+
+	for(i=1;i<=n;i+=2)
+	{	cout<<i+1<<" "<<i<<" ";
+	}
+
+	return 0;
+}
